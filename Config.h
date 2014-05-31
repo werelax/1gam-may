026f3003f240869f1f75f446d831b771af58888e
Elías Alonso <eliasagc@gmail.com>
2014-05-31 17:04:45 +0200

    Config implementation file added
    Because it feels to weird to put everything in the .h file. Don't really
    know what is the standard way to do things in that front. The .cpp file
    is a little bit more trouble because the Makefile has to be updated (by
    the way, not sure if I'm doing the Makefile thing right either...).

--- Config.h
+++ Config.h
@@ -9,62 +9,19 @@
 class Config
 {
 public:
-    Config(const char* filename): fileContents(readFileContents(filename)) {
-        json_error_t error;
-        root = json_loads(fileContents.c_str(), 0, &error);
-        if (!root) {
-            std::cout << "Unable to load JSON from: " << filename << std::endl;
-        } else {
-            std::cout << "JSON read succesfully!!" << std::endl;
-        }
-    }
-    ~Config() {
-        json_decref(root);
-    }
+    Config(const char* filename);
 
-    std::string getOptionAsString(std::string optionName) {
-        std::string optionNameCopy = optionName;
-        json_t* option = getPathReference(root, optionNameCopy);
-        if (option) {
-            return std::string(json_string_value(option));
-        } else {
-            return "";
-        }
-    }
+    ~Config();
 
-    void getOptionList(const char* optionName) {
-    }
+    std::string getOptionAsString(std::string optionName);
+
+    void getOptionList(const char* optionName);
 
 private:
 
-    json_t* getPathReference(json_t* root, std::string path) {
-        size_t prev = 0;
-        size_t next = 0;
-        json_t* reference = root;
-        while ((next = path.find_first_of(".", prev)) != std::string::npos &&
-               reference != NULL) {
-            reference = json_object_get(reference,
-                                        path.substr(prev, next - prev).c_str());
-            prev = next + 1;
-        }
-        return json_object_get(reference, path.substr(prev).c_str());
-    }
+    json_t* getPathReference(json_t* root, std::string path);
 
-    std::string readFileContents(const char* filename) {
-        std::ifstream in(filename, std::ios::in | std::ios::binary);
-        if (in) {
-            std::string contents;
-            in.seekg(0, std::ios::end);
-            contents.resize(in.tellg());
-            in.seekg(0, std::ios::beg);
-            in.read(&contents[0], contents.size());
-            in.close();
-            return contents;
-        } else {
-            std::cout << "Unable to open file: " << filename << std::endl;
-            return NULL;
-        }
-    }
+    std::string readFileContents(const char* filename);
 
     std::string fileContents;
     json_t* root;
